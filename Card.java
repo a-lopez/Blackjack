ddd631afd78c2964354e9776c6a430765e7a931d
a-lopez <alejandro.lopez127668@gmail.com>
2017-05-19 12:08:32 -0400

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,4 +1,4 @@
-// Fig. 7.9: Card.java
+
 // Card class represents a playing card.
 
 public class Card 
@@ -39,17 +39,3 @@ public class Card
 } // end class Card
 
 
-/**************************************************************************
- * (C) Copyright 1992-2012 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
- *************************************************************************/
